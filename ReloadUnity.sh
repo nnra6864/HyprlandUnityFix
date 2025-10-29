4f178702b3309bc84f6495431e901562d3d0434b
nnra6864 <enenragd@gmail.com>
2025-10-29 15:35:29 +0100

    Adjusted the sleep time to 0.5 to work with most terminals, e.g. ghostty

--- ReloadUnity.sh
+++ ReloadUnity.sh
@@ -9,7 +9,7 @@ else
     notify-send "Reload Unity" "Couldn't reload, \$TERMINAL is not set in env vars"
     exit 1;
 fi
-sleep 0.25
+sleep 0.5
 hyprctl dispatch setfloating pid:$TERM_PID
 hyprctl dispatch resizewindowpixel exact 1 1, pid:$TERM_PID
 hyprctl dispatch centerwindow pid:$TERM_PID
